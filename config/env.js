403ffa07546cde5d492c4a0d0e53f78ccf7558ed
Nirasha <demelwgnd.22@uom.lk>
2025-11-05 09:28:17 +0530

    feat(env): export JWT_SECRET and JWT_EXPIRES_IN from environment variables feat(package): add bcryptjs and jsonwebtoken dependencies

--- config/env.js
+++ config/env.js
@@ -15,4 +15,4 @@ for (const p of candidatePaths) {
 	}
 }
 
-export const { PORT, NODE_ENV, DB_URI } = process.env;
+export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } = process.env;
