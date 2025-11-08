8cb25f8aa22279855a87db4d772843473656d79f
Nirasha <demelwgnd.22@uom.lk>
2025-11-08 22:11:38 +0530

    feat(workflow): fix workflow client initialization and add workflow router

--- config/upstash.js
+++ config/upstash.js
@@ -1,9 +1,9 @@
 import { Client as workflow } from "@upstash/workflow";
 import { QSTASH_TOKEN, QSTASH_URL } from "./env";
 
-export const workflowClient = workflowClientlient(clientConfig: {
+export const workflowClient = new workflow({
     baseUrl: QSTASH_URL,
     token: QSTASH_TOKEN,
 });
 
-export default client;
+export default workflowClient;
