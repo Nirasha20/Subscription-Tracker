bd09b8698f47f50d86fbfe905907c81dd7f926aa
Nirasha <demelwgnd.22@uom.lk>
2025-11-09 09:26:56 +0530

    feat(workflow): refactor sendReminders function to improve reminder scheduling logic

--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -2,7 +2,7 @@ import dayjs from "dayjs";
 import Subscription from "../models/subscription.model.js";
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-
+const REMINDERS = [7, 5, 2, 1]; // days before renewal
 const { serve } = require("@upstash/workflow/express");
 export const sendReminders = serve( async (context) => {
     const { subscriptionId } = context.requestPayload;
@@ -12,15 +12,33 @@ export const sendReminders = serve( async (context) => {
         return ;
     const renewalDate = dayjs(subscription.renewalDate);
     if(renewalDate.isBefore(dayjs())) {
-    console.log('Renewal date has passed for subscription ${subscriptionId}.Stoping workflow')
-    return;
-
+        console.log(`Renewal date has passed for subscription ${subscriptionId}. Stopping workflow`);
+        return;
     }
-    
+
+    for(const daysBefore of REMINDERS) {
+        const reminderDate = renewalDate.subtract(daysBefore, 'day');
+        
+        // schedule or send reminder for reminderDate
+        if(reminderDate.isAfter(dayjs())) {
+            await sleepUntilReminder(context, `${daysBefore} day`, reminderDate);
+        }
+        await triggerReminder(context, `${daysBefore} days before`);
+    }
 });
 
 const fetchSubscription = async (context, subscriptionId) => {
     return await context.run('get subscription', () => {
         return Subscription.findById(subscriptionId).populate('user', 'name email');
+    })
+}
+const sleepUntilReminder = async (context, label, date  ) => {
+    console.log(`Sleeping until ${label} reminder at ${date}`);
+    await context.sleepUntil(label, date.toDate());
+}
+const triggerReminder =async (context, label) => {
+    return await context.run(label, () => {
+        console.log(`Triggering ${label} reminder`);
+
     })
 }
